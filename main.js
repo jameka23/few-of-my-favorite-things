d5ff8c51428f80ddb89ffe4438528620c3350cd9
Jameka Echols <jameka.echols@gmail.com>
2019-03-06 16:00:30 -0600

    finished practice1

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("End of day lesson.")
+const things = document.getElementById("things")
+const locations = document.getElementById("location")
+const wishlist = document.querySelector(".wishlist")
+
+document.getElementById("saveButton").addEventListener("click", function(event){
+    wishlist.innerHTML = `I can purchase ${things.value} at ${locations.value}.`
+})
